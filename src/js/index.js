f26a07a21273799f7bc767fdbb5483e0c08094fd
plmrlari <109881943+plmrlari@users.noreply.github.com>
2023-01-22 23:26:57 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+
+const botaoTrailer = document.querySelector(".botao-trailer");
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+const linkDoVideo = video.src;
+
+botaoTrailer.addEventListener("click",() => {
+    modal.classList.add("aberto");
+    video.setAttribute("src", linkDoVideo);
+});
+
+const botaoFecharModal = document.querySelector(".fechar-modal");
+
+botaoFecharModal.addEventListener("click", () => {
+ modal.classList.remove("aberto");
+ video.setAttribute("src", "");
+});
+
+
+
+
